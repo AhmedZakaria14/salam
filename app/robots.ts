36539e8b9a4be78df611abf80dd794bdefc93464
AhmedZakaria14 <azeco77@gmail.com>
2026-08-08 16:53:27 +0300

    seo: point robots sitemap to production domain

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.APP_URL || 'https://xn-----ctdcm0aqck5plajre8a.com';
+  const baseUrl = 'https://www.mandoubsalam5g.com';
 
   return {
     rules: {
